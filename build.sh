5f889acb41bca94261c8391dcd6222cdeb7f4f04
sam <sam@sam-hp-linux.lan>
2024-11-05 11:58:30 -0700

    update build script to just build the oneshot & interactive

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 sbcl --load main.lisp --eval "(sb-ext:save-lisp-and-die #P\"./play-chesskers\" :toplevel #'chesskers::play-game :executable t)"
-sbcl --load main.lisp --eval "(sb-ext:save-lisp-and-die #P\"./chesskers-computer-executable\" :toplevel #'chesskers::for-python-as-subprocess :executable t)"
+sbcl --load main.lisp --eval "(sb-ext:save-lisp-and-die #P\"./chesskers-computer-executable\" :toplevel #'chesskers::for-python-oneshot :executable t)"
